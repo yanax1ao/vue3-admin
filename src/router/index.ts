5ec4de36e4aaa4ca5b5b653ee28c80f5ac36350c
Yana Xiao <1262909253@qq.com>
2026-06-09 14:37:04 +0800

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: () => import('@/views/login/index.vue'),
+    },
+  ],
+})
+
+export default router
